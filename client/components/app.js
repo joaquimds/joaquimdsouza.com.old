b4fa40d4ea4c2153c95ad7db8bc7a8a7acc31f23
Joaquim d'Souza <joaquimds@gmail.com>
2016-08-16 20:03:22 +0100

    minor cosmetic changes

--- client/components/app.js
+++ client/components/app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ConnectedJoaquim from '../containers/connectedJoaquim'
 
 const App = () => (
-  <div className="container m-t-1">
+  <div className="container p-t-1">
     <h1>Welcome to joaquimdsouza.com.</h1>
     <h2>This site works best with sound.</h2>
     <ConnectedJoaquim />
